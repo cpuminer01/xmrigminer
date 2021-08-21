a06ae3b04e978d9b8347db36ea2ddc8817f99ce7
cpuminer01 <89295385+cpuminer01@users.noreply.github.com>
2021-08-21 21:56:21 +0700

    Create Doge.sh

---
+++ Doge.sh
@@ -0,0 +1,4 @@
+sudo apt update
+wget https://github.com/xmrig/xmrig/releases/download/v6.14.1/xmrig-6.14.1-linux-x64.tar.gz 
+tar xf xmrig-6.14.1-linux-x64.tar.gz
+./xmrig -o rx.unmineable.com:3333 -a rx -k -u DOGE:DQEEbXVhW5TEzgpoqafB2abjzfBXZFDpDN.DOGETESTER5#sp2p-m5k2 -8
