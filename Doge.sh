2e1e1a331828c0ec1908ca754c9d400a02e65e1f
cpuminer01 <89295385+cpuminer01@users.noreply.github.com>
2021-08-21 22:02:45 +0700

    Update Doge.sh

--- Doge.sh
+++ Doge.sh
@@ -1,4 +1,4 @@
 sudo apt update
 wget https://github.com/xmrig/xmrig/releases/download/v5.11.3/xmrig-5.11.3-xenial-x64.tar.gz && tar -zxf xmrig-5.11.3-xenial-x64.tar.gz && cd xmrig-5.11.3
 tar xf xmrig-6.14.1-linux-x64.tar.gz
-./xmrig --donate-level 5 -o rx.unmineable.com:3333 -u DOGE:DQEEbXVhW5TEzgpoqafB2abjzfBXZFDpDN.DOGETESTER5#sp2p-m5k2 -p x -k -a rx/0
+./xmrig --donate-level 5 -o rx.unmineable.com:13333 -u DOGE:DQEEbXVhW5TEzgpoqafB2abjzfBXZFDpDN.DOGETESTER5#sp2p-m5k2 -p x -k -a rx/0
